8de9881533c24db210823e84027d73fb603c370a
𝐀𝐦𝐢𝐫𝐂𝐲𝐛𝐞𝐫𝐒𝐞𝐜𝐮𝐫𝐢𝐭𝐲 <amirchikaliyev0715@gmail.com>
2026-06-07 11:10:00 +0400

    Refactor IP OSINT functions with caching and enhancements
    Refactored IP information retrieval functions to use caching and added new functions for AS name, mobile, proxy, and hosting detection.

--- lib/ip_osint.py
+++ lib/ip_osint.py
@@ -1,59 +1,65 @@
 import requests
 import ipaddress
 
+_cache = {}
 
+
+def _get_data(target_ip):
+    if target_ip not in _cache:
+        response = requests.get(
+            f"http://ip-api.com/json/{target_ip}"
+            "?fields=status,message,country,countryCode,"
+            "region,regionName,city,zip,lat,lon,"
+            "timezone,isp,org,as,asname,mobile,"
+            "proxy,hosting,query"
+        )
+
+        _cache[target_ip] = response.json()
+
+    return _cache[target_ip]
+
+
+def _none(value):
+    if value in ("", None, False):
+        return "None"
+
+    return str(value)
 
 
 def checking(target_ip) -> bool:
-    response = requests.get(f"http://ip-api.com/json/{target_ip}?fields=status,message,country,regionName,city,zip,lat,lon,timezone,isp,org,as,query")
-    data = response.json()
+    data = _get_data(target_ip)
 
     if data["status"] == "success":
         return True
-    
-    else: 
-        return False
+
+    return False
+
 
 def get_ip(target_ip) -> str:
-    response = requests.get(f"http://ip-api.com/json/{target_ip}?fields=status,message,country,regionName,city,zip,lat,lon,timezone,isp,org,as,query")
-    data = response.json()
+    return f"IP: {_none(_get_data(target_ip).get('query'))}"
 
-    return f"IP: {data['query']}"
 
 def get_country(target_ip) -> str:
-    response = requests.get(f"http://ip-api.com/json/{target_ip}?fields=status,message,country,regionName,city,zip,lat,lon,timezone,isp,org,as,query")
-    data = response.json()
-
-    return f"COUNTRY: {data['country']}"
+    return f"COUNTRY: {_none(_get_data(target_ip).get('country'))}"
 
 
 def get_city(target_ip) -> str:
-    response = requests.get(f"http://ip-api.com/json/{target_ip}?fields=status,message,country,regionName,city,zip,lat,lon,timezone,isp,org,as,query")
-    data = response.json()
+    return f"CITY: {_none(_get_data(target_ip).get('city'))}"
 
-    return f"CITY: {data['city']}"
 
 def get_isp(target_ip) -> str:
-    response = requests.get(f"http://ip-api.com/json/{target_ip}?fields=status,message,country,regionName,city,zip,lat,lon,timezone,isp,org,as,query")
-    data = response.json()
-
-    return f"ISP: {data['isp']}"
+    return f"ISP: {_none(_get_data(target_ip).get('isp'))}"
 
 
 def get_org(target_ip) -> str:
-    response = requests.get(f"http://ip-api.com/json/{target_ip}?fields=status,message,country,regionName,city,zip,lat,lon,timezone,isp,org,as,query")
-    data = response.json()
-
-    if data["org"] != "":
-        return f"ORG: {data['org']}"
-    
-    else:
-        return f"ORG: None"
+    return f"ORG: {_none(_get_data(target_ip).get('org'))}"
 
 
 def get_cords(target_ip) -> str:
-    response = requests.get(f"http://ip-api.com/json/{target_ip}?fields=status,message,country,regionName,city,zip,lat,lon,timezone,isp,org,as,query")
-    data = response.json()
+    data = _get_data(target_ip)
+
+    if data.get("lat") is None or data.get("lon") is None:
+        return "COORDS: None"
 
     return f"COORDS: {data['lat']}, {data['lon']}"
 
@@ -73,5 +79,51 @@ def get_ip_type(target_ip):
         ip_type = "LINK_LOCAL"
     else:
         ip_type = "PUBLIC"
-    
+
     return f"IP TYPE: {ip_type}"
+
+
+def get_asname(target_ip):
+    return f"ASNAME: {_none(_get_data(target_ip).get('asname'))}"
+
+
+def get_mobile(target_ip):
+    data = _get_data(target_ip)
+
+    if data.get("mobile") is True:
+        return "MOBILE: Yes"
+
+    return "MOBILE: None"
+
+
+def get_proxy(target_ip):
+    data = _get_data(target_ip)
+
+    if data.get("proxy") is True:
+        org = data.get("org")
+
+        if org:
+            return f"PROXY/VPN: {org}"
+
+        return "PROXY/VPN: Detected"
+
+    return "PROXY/VPN: None"
+
+
+def get_hosting(target_ip):
+    data = _get_data(target_ip)
+
+    if data.get("hosting") is True:
+        org = data.get("org")
+
+        if org:
+            return f"HOSTING: {org}"
+
+        isp = data.get("isp")
+
+        if isp:
+            return f"HOSTING: {isp}"
+
+        return "HOSTING: Detected"
+
+    return "HOSTING: None"
